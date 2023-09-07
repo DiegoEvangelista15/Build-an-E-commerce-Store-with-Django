946e7642464a949b6879c7897c2d0abbc6a47a8b
DiegoEvangelista15 <diegoapevangelista@gmail.com>
2023-09-07 09:27:45 -0300

    email account config views and templates adjustments

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -135,3 +135,12 @@ MEDIA_ROOT = BASE_DIR / 'static/media'  # não colocar / antes do static
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'x@gmail.com'
+EMAIL_HOST_PASSWORD = '31290-1293-0912'  # APENAS EXEMPLO MAS SE DEVE COLOCAR O SEU EMAIL
+
